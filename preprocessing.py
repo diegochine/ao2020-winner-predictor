0202ef77f47c5235120b5ff5741c65aa24f3185e
Giorgia Campardo <giorgia.campardo@azserve.com>
2020-01-14 14:54:18 +0200

    splittted preprocess_data in unify_data

--- preprocessing.py
+++ preprocessing.py
@@ -53,23 +53,11 @@ DATA_TYPES = {
     'LBHand': float
 }
 
-def preprocess_data(min_date=2011,
-                    max_date=2019,
-                    features_to_drop=[], 
-                    missing_values="drop", 
-                    drop_first=False):
-    """
-    Processes raw data and returns a tuple (X, Y) where X is the cleaned dataset and Y is the array of labels.
-    """
-    # Loads data for the given years
-    if max_date > 2019 or min_date < 2011:
-        raise ValueError("Wrong date parameter")
-    df = pd.read_csv("data/" + str(min_date) + ".csv", encoding='utf-8-sig', dtype=DATA_TYPES)
-    for year in range (min_date + 1, max_date + 1):
-        filename = "data/" + str(year) + ".csv"
-        df = pd.concat((df, pd.read_csv(filename, encoding='utf-8-sig', dtype=DATA_TYPES)))
-    
-    # Sort by date to calculate ELO
+def unify_data(df,
+               features_to_drop=[], 
+               missing_values="drop", 
+               drop_first=False):
+     # Sort by date to calculate ELO
     X = df.sort_values(by='Date')
     
     # Drop unuseful columns
@@ -132,28 +120,55 @@ def preprocess_data(min_date=2011,
     if 'Surface' not in features_to_drop:
         X = pd.get_dummies(X, prefix=['Surface_'], columns=['Surface'], drop_first=drop_first)
     
-    # Generate labels
-    Y = np.concatenate([np.ones(X.shape[0], dtype=int), np.zeros(X.shape[0], dtype=int)])
-    
-    # Duplicate data with swapped columns
-    tmp = X.copy()
-    cols_to_swap = ['WRank', 'LRank', 'MaxW', 'MaxL',  'AvgW',  'AvgL', 'WPts', 'LPts',
-                    'WElo', 'LElo', 'WSurfElo', 'LSurfElo', 'WHand', 'LHand', 'WBHand', 'LBHand']
-    cols_to_swap = [f for f in cols_to_swap if f not in features_to_drop]
-    cols_swapped = ['LRank', 'WRank', 'MaxL', 'MaxW',  'AvgL',  'AvgW', 'LPts', 'WPts',
-                    'LElo', 'WElo', 'LSurfElo', 'WSurfElo', 'LHand', 'WHand', 'LBHand', 'WBHand']
-    cols_swapped = [f for f in cols_swapped if f not in features_to_drop]
-    
-    tmp[cols_to_swap] = tmp[cols_swapped]
-    tmp.index = np.array(range(X.shape[0] + 1, X.shape[0] * 2 + 1))
-    X = pd.concat((X, tmp))
-    
     # Generate new columns
     #X['GreaterRank'] = (X['WRank'] < X['LRank']).astype(int)
     
     # Rename columns
     X = X.rename(columns={'WRank':'P1Rank', 'LRank':'P2Rank', 
                           'MaxW':'MaxP1', 'MaxL':'MaxP2', 
-                          'AvgW':'AvgP1', 'AvgL':'AvgP2'})
+                          'AvgW':'AvgP1', 'AvgL':'AvgP2',
+                          'WPts':'P1Pts', 'LPts':'P2Pts',
+                          'WElo':'P1Elo', 'LElo':'P2Elo',
+                          'WSurfElo':'P1SurfElo', 'LSurfElo':'P2SurfElo',
+                          'WHand':'P1Hand', 'LHand':'P2Hand',
+                          'WBHand':'P1BHand', 'LBHand':'P2BHand'})
+    
+    return X
+    
+
+def preprocess_data(min_date=2011,
+                    max_date=2019,
+                    features_to_drop=[], 
+                    missing_values="drop", 
+                    drop_first=False):
+    """
+    Processes raw data and returns a tuple (X, Y) where X is the cleaned dataset and Y is the array of labels.
+    """
+    # Loads data for the given years
+    if max_date > 2019 or min_date < 2011:
+        raise ValueError("Wrong date parameter")
+    df = pd.read_csv("data/" + str(min_date) + ".csv", encoding='utf-8-sig', dtype=DATA_TYPES)
+    for year in range (min_date + 1, max_date + 1):
+        filename = "data/" + str(year) + ".csv"
+        df = pd.concat((df, pd.read_csv(filename, encoding='utf-8-sig', dtype=DATA_TYPES)))
+        
+    X = unify_data(df, features_to_drop, missing_values, drop_first)
+   
+    # Generate labels
+    Y = np.concatenate([np.ones(X.shape[0], dtype=int), np.zeros(X.shape[0], dtype=int)])
+    
+    # Duplicate data with swapped columns
+    tmp = X.copy()
+    cols_to_swap = ['P1Rank', 'P2Rank', 'MaxP1', 'MaxP2',  'AvgP1',  'AvgP2', 'P1Pts', 'P2Pts',
+                    'P1Elo', 'P2Elo', 'P1SurfElo', 'P2SurfElo', 'P1Hand', 'P2Hand', 'P1BHand', 'P2BHand']
+    cols_to_swap = [f for f in cols_to_swap if f not in features_to_drop]
+    cols_swapped = ['P2Rank', 'P1Rank', 'MaxP2', 'MaxP1',  'AvgP2',  'AvgP1', 'P2Pts', 'P1Pts',
+                    'P2Elo', 'P1Elo', 'P2SurfElo', 'P1SurfElo', 'P2Hand', 'P1Hand', 'P2BHand', 'P1BHand']
+    cols_swapped = [f for f in cols_swapped if f not in features_to_drop]
+    
+    tmp[cols_to_swap] = tmp[cols_swapped]
+    tmp.index = np.array(range(X.shape[0] + 1, X.shape[0] * 2 + 1))
+    X = pd.concat((X, tmp))
+    
     return X, Y
 
