0f2c9f511696b9a0747418dd7cfb0a8c5a69cf97
Giorgia Campardo <giorgia.campardo@azserve.com>
2020-01-14 16:23:32 +0200

    added computing elo

--- preprocessing.py
+++ preprocessing.py
@@ -53,6 +53,32 @@ DATA_TYPES = {
     'LBHand': float
 }
 
+def compute_elo_rankings(data):
+    """
+    Given the list on matches in chronological order, for each match, computes 
+    the elo ranking of the 2 players at the beginning of the match
+    """
+    print("Elo rankings computing...")
+    players=list(pd.Series(list(data.Winner)+list(data.Loser)).value_counts().index)
+    elo=pd.Series(np.ones(len(players))*1500,index=players)
+    ranking_elo=[(1500,1500)]
+    for i in range(1,len(data)):
+        w=data.iloc[i-1,:].Winner
+        l=data.iloc[i-1,:].Loser
+        elow=elo[w]
+        elol=elo[l]
+        pwin=1 / (1 + 10 ** ((elol - elow) / 400))    
+        K_win=32
+        K_los=32
+        new_elow=elow+K_win*(1-pwin)
+        new_elol=elol-K_los*(1-pwin)
+        elo[w]=new_elow
+        elo[l]=new_elol
+        ranking_elo.append((elo[data.iloc[i,:].Winner],elo[data.iloc[i,:].Loser])) 
+    ranking_elo=pd.DataFrame(ranking_elo,columns=["elo_winner","elo_loser"])    
+    ranking_elo["proba_elo"]=1 / (1 + 10 ** ((ranking_elo["elo_loser"] - ranking_elo["elo_winner"]) / 400))   
+    return ranking_elo
+
 def unify_data(df,
                features_to_drop=[], 
                missing_values="drop", 
@@ -122,6 +148,10 @@ def unify_data(df,
     
     # Generate new columns
     #X['GreaterRank'] = (X['WRank'] < X['LRank']).astype(int)
+    r = compute_elo_rankings(full_dataset)
+    X['WEloCalc'] = r['elo_winner']
+    X['LEloCalc'] = r['elo_loser']
+    X['PElo'] = r['proba_elo']
     
     return X
     
